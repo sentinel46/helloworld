5c13dbb9c4a9fdd3751cfeb54aff142a882361ba
sentinel46 <altdaedroth@gmail.com>
2018-03-14 22:39:41 +0300

    added help info via boost::program_options

--- main.cpp
+++ main.cpp
@@ -1,15 +1,44 @@
 #include <iostream>
 #include <tuple>
 
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/variables_map.hpp>
+
 #include "lib.h"
 
-int main()
+int main(int argc, char *argv[])
 {
-    std::cout << "Hello World!" << std::endl;
-    int major = 0;
-    int minor = 0;
-    int patch = 0;
-    std::tie(major, minor, patch) = version();
-    std::cout << "Version " << major << "." << minor << "." << patch << std::endl;
+    boost::program_options::options_description general("General options");
+
+    try
+    {
+        general.add_options()
+                ("help,h", "produce help message")
+                ("version,v", "output the version number");
+        boost::program_options::variables_map vm;
+        boost::program_options::store(boost::program_options::parse_command_line(argc, argv, general), vm);
+
+        std::cout << "Hello World!" << std::endl;
+
+
+        if (vm.count("help"))
+        {
+            std::cout << general;
+        }
+        else if (vm.count("version"))
+        {
+            auto major = 0;
+            auto minor = 0;
+            auto patch = 0;
+            std::tie(major, minor, patch) = version();
+            std::cout << "version " << major << "." << minor << "." << patch << std::endl;
+        }
+    }
+    catch (std::exception &e)
+    {
+        std::cout << general;
+    }
+
     return 0;
 }
